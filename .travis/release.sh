4f8a03c03336a64bd13c6e290d5740c6a354764f
Oliver Messner <messner@synyx.de>
2025-01-07 13:11:31 +0100

    Update release.sh

--- .travis/release.sh
+++ .travis/release.sh
@@ -5,5 +5,5 @@ set -e
 echo "Ensuring that pom <version> matches $TRAVIS_TAG"
 ./mvnw --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG
 
-echo "Uploading to contargo-oss and github"
+echo "Uploading to github"
 ./mvnw clean deploy --settings .travis/settings.xml -DskipTests=true --batch-mode --update-snapshots -Prelease
